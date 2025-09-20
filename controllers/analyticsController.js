09b5d49d423c5623aa7d4c1e26b9689c7b80a4e0
Soumyadeepbasak037 <soumyadeep.basak06@gmail.com>
2025-09-20 20:56:24 +0530

    Analytics

--- controllers/analyticsController.js
+++ controllers/analyticsController.js
@@ -21,6 +21,36 @@ exports.getSalesSummary = async (req, res) => {
 
 exports.getTopProducts = async (req, res) => {
   try {
-    const query = ``;
-  } catch (err) {}
+    const query = ` SELECT p.name, SUM(oi.quantity) as sold, SUM(oi.quantity * oi.price) as revenue
+      FROM order_items oi
+      JOIN products p ON p.id = oi.product_id
+      GROUP BY p.id
+      ORDER BY sold DESC
+      LIMIT 10`;
+
+    const topProductsresults = await db.query(query);
+    const topProducts = topProductsresults.rows;
+
+    res.json({ topProducts });
+  } catch (err) {
+    res.status(500).json({ message: "Error fetching details" });
+  }
+};
+
+exports.getTopCustomers = async (req, res) => {
+  try {
+    const query = ` SELECT u.username, u.email, SUM(o.total_price) as "Total_Amount", COUNT(o.id) as "Orders"
+    FROM users u
+    JOIN orders o ON u.id = o.user_id
+    GROUP BY u.id
+    ORDER BY SUM(o.total_price) DESC
+    LIMIT 10`;
+
+    const topCustomersresults = await db.query(query);
+    const topCustomers = topCustomersresults.rows;
+
+    res.json({ topCustomersresults });
+  } catch (err) {
+    res.status(500).json({ message: "Error fetching top customers" });
+  }
 };
