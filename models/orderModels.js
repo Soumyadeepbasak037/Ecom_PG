b1accea61802694abbe3a455f4766a7d5358b63a
Soumyadeepbasak037 <soumyadeep.basak06@gmail.com>
2025-09-27 19:06:06 +0530

    orderModels

--- models/orderModels.js
+++ models/orderModels.js
@@ -1,1 +1,13 @@
 const db = require("../config/db");
+const Products = require("../models/productModels");
+
+exports.findProductById = (productId) => {
+  return db.query(`SELECT * FROM products WHERE id = $1`, [productId]);
+};
+
+exports.findCartByID = (userId, productId) => {
+  return db.query(
+    `SELECT * FROM cart_items WHERE user_id = $1 AND product_id = $2`,
+    [userId, productId]
+  );
+};
